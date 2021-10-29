75b15bcdc1524e340e0ae984314aa0f6f643e296
Hramcov Semen <gerasm666999@gmail.com>
2021-10-29 14:30:44 +0500

    finali commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-
+        int deposit =1105;
+        int newBalance;
+        if (deposit > 1000) {
+            newBalance = balance + deposit + deposit / 100;
+        } else {
+            newBalance = balance + deposit;
+        }
+        System.out.println(newBalance);
     }
 }
